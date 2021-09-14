805d0eb512a6def59542b6b7299533b295cc8937
pavlvs <pavlvsxavier@gmail.com>
2021-09-14 15:44:47 +0100

    added chaclk and colored directories in nls

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs')
 const util = require('util')
+const chalk = require('chalk')
 
 // Method #2
 // const lstat = util.promisify(fs.lstat)
@@ -22,7 +23,11 @@ fs.readdir(process.cwd(), async (err, filenames) => {
     for (let stats of allStats) {
         const index = allStats.indexOf(stats)
 
-        console.log(filenames[index], stats.isFile())
+        if (stats.isFile()) {
+            console.log(filenames[index])
+        } else {
+            console.log(chalk.blueBright(filenames[index]))
+        }
     }
 })
 
